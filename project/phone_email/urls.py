7b068380406bf9da70f6f5434272665cf63559af
Devendra <dev@phone.email>
2024-06-20 11:45:01 +0530

    update

--- project/phone_email/urls.py
+++ project/phone_email/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', index, name='index'),
-    path('', views.dashboard, name='dashboard'),
+    path('', views.index, name='index'),
+    # path('', views.dashboard, name='dashboard'),
 
 ]
--- project/phone_email/views.py
+++ project/phone_email/views.py
@@ -1,15 +1,8 @@
 from django.shortcuts import render
-from django.http import JsonResponse
-import requests
-from django.views.decorators.csrf import csrf_exempt
-from django.shortcuts import redirect
 
-def dashboard(request):
-    # Render the dashboard page
-    
-    response = render(request, 'home.html')
+def index(request):
+    # Render the index page    
+    response = render(request, 'index.html')
     # Set the Cross-Origin-Opener-Policy header for handling popups
     response['Cross-Origin-Opener-Policy'] = 'same-origin-allow-popups'
     return response
-    
-    # return render(request, 'sign_in.html')
